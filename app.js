d1eba7ae1141dd1798c19aa4031eba53ecd7f924
Alan Zacharia <alanzacaharia@gmail.com>
2023-12-12 16:39:57 +0530

    email

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 var server = http.createServer(app);
 server.listen(port);
-server.on('error', onError);
+server.on('error', onError);        
 server.on('listening', onListening);
 
 function normalizePort(val) {
